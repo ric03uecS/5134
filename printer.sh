844762b0abfce5091512dafe9b4db6cc048865ae
Himanshu Aggarwal <hemanshu0503@gmail.com>
2017-02-08 14:15:24 +0530

    Update printer.sh

--- printer.sh
+++ printer.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 x=$((0));
 n=$((0));
-while [ $(($n < 100)) == 1 ];
+while [ $(($n < 10000)) == 1 ];
   do 
     n=$(($n + 1));
-    while [ $(($x < 100)) == 1 ];
+    while [ $(($x < 100000)) == 1 ];
       do
         x=$(($x + 1));
         echo -n $x >> iterator.txt;
