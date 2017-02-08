ec617bc67a9625504747f9c4cc5ba99ec686a1e1
Himanshu Aggarwal <hemanshu0503@gmail.com>
2017-02-08 14:12:40 +0530

    Create printer.sh

---
+++ printer.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+x=$((0));
+n=$((0));
+while [ $(($n < 100)) == 1 ];
+  do 
+    n=$(($n + 1));
+    while [ $(($x < 100)) == 1 ];
+      do
+        x=$(($x + 1));
+        echo -n $x >> iterator.txt;
+    done;
+    x=0
+    echo "">>iterator.txt;
+done
